df789c46c36082946d46e5165d4b7642dcdfdc9c
Tinio-Vitali <agustinvitali12@gmail.com>
2024-08-15 15:47:27 +0300

    Fig. 2 updated

--- Exploratory/Scenario_ES/graph_networks.R
+++ Exploratory/Scenario_ES/graph_networks.R
@@ -10,7 +10,8 @@ setwd("/Users/agustin/Desktop/Papers/Norwood_farm/Norwood_Tinio")
 
 Norwood_farm<-readRDS("Data/Norwood_farm.RData") #read multilayer object to get the node's list
 
-nodes<- Norwood_farm$nodes #node list with attributes
+nodes<- Norwood_farm$nodes %>% 
+  mutate(taxon = str_replace(taxon, "Flower-visiting", "Flower visitor")) #node list with attributes
 edge_list<-read.csv("Data/Land_use_rat_edgelist_weighted_CP_intense.csv", sep =",") %>% 
                     mutate(weight == 1) #edge list
 
@@ -23,9 +24,9 @@ E<- graph_from_edgelist(as.matrix(E_edge), directed = FALSE) #create igraph obje
 
 S <- vcount(E)#number of species
 L <- ecount(E) #number of links
-C <- ecount(E) / vcount(E)^2 
+#C <- ecount(E) / vcount(E)^2 
 
-color_trophic <-tibble(taxon = c("Plant","Crop","Flower-visiting","Aphid","Primary aphid parasitoid","Secondary aphid parasitoid",
+color_trophic <-tibble(taxon = c("Plant","Crop","Flower visitor","Aphid","Primary aphid parasitoid","Secondary aphid parasitoid",
                                  "Leaf-miner parasitoid","Seed-feeding insect","Seed-feeding bird",
                                  "Seed-feeding rodent","Butterfly","Insect seed-feeder parasitoid","Rodent ectoparasite"),
                        color = c("#33a02c","#b15928","#a6cee3","#1f78b4","#b2df8a","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#cab2d6",
@@ -38,7 +39,7 @@ V(E)$taxon<-nodes$taxon #create taxon attribute
 vcol=V(E)$taxon
 vcol[vcol== "Plant"]="#33a02c"
 vcol[vcol=="Crop"]="#b15928"
-vcol[vcol=="Flower-visiting"]="#a6cee3"
+vcol[vcol=="Flower visitor"]="#a6cee3"
 vcol[vcol== "Aphid"]="#1f78b4"
 vcol[vcol=="Primary aphid parasitoid"]="#b2df8a"
 vcol[vcol=="Secondary aphid parasitoid"]="#fb9a99"
@@ -56,26 +57,25 @@ V(E)$color=vcol
  
 ##Plot
 
-png("Graphs/E_network.png", width = 800, height = 600)
+#png("Graphs/E_network.png", width = 800, height = 600)
+pdf("Graphs/E_network.pdf", width = 5, height = 7)
 E_network<-plot(E,edge.arrow.size=.5, vertex.color= V(E)$color, vertex.size=4, vertex.label=NA,
      vertex.frame.color="black", vertex.label.color="black", 
      vertex.label.cex=.5, vertex.label.dist=0.5, edge.curved=0.2)
 
 dev.off()
 
-png("Graphs/Legend_network.png", width = 800, height = 600)
+pdf("Graphs/Legend_network.pdf", width = 4, height = 5)
 legend_network<-plot(NULL ,xaxt='n',yaxt='n',bty='n',ylab='',xlab='', xlim=0:1, ylim=0:1)
-
-legend("bottom", legend = color_trophic$taxon, pch=21,
-       col= "black",
+legend("bottom", legend = color_trophic$taxon, pch = 21,
+       col = "black",
        pt.bg = color_trophic$color,
-       pt.cex=2, cex=1, bty="n")
+       pt.cex = 2, cex = 1, bty = "n", ncol = 2, title = "Trophic guild")
 
 dev.off()
 
 
 ###### MODERATE
-
 MCP_edge <- edge_list %>% filter(management == "M") %>% select(node_from,node_to)
 
 #Create igraph object
@@ -88,7 +88,7 @@ M_CP2<- delete_vertices(M_CP, isolated_nodes)
 
 S <- vcount(M_CP2)#number of species
 L <- ecount(M_CP2) #number of links
-C <- ecount(M_CP2) / vcount(M_CP2)^2  #connectance
+#C <- ecount(M_CP2) / vcount(M_CP2)^2  #connectance
 
 ## assign attributes according to the taxon
 nodes_in_network<- nodes %>% filter(node_id%in%MCP_edge$node_from | #create the nodelist according to the nodes present in the network
@@ -100,7 +100,7 @@ V(M_CP2)$taxon<-nodes_in_network$taxon #create taxon attribute
 vcol=V(M_CP2)$taxon
 vcol[vcol== "Plant"]="#33a02c"
 vcol[vcol=="Crop"]="#b15928"
-vcol[vcol=="Flower-visiting"]="#a6cee3"
+vcol[vcol=="Flower visitor"]="#a6cee3"
 vcol[vcol== "Aphid"]="#1f78b4"
 vcol[vcol=="Primary aphid parasitoid"]="#b2df8a"
 vcol[vcol=="Secondary aphid parasitoid"]="#fb9a99"
@@ -115,7 +115,8 @@ V(M_CP2)$color=vcol
 
 
 #Plot 
-png("Graphs/M_CP_network.png", width = 800, height = 600)
+#png("Graphs/M_CP_network.png", width = 800, height = 600)
+pdf("Graphs/M_network.pdf", width = 5, height = 7)
 M_CPplot<-plot(M_CP2,edge.arrow.size=.5, vertex.color= V(M_CP2)$color, vertex.size=6, vertex.label=NA,
                vertex.frame.color="black", vertex.label.color="black", 
                #vertex.label.color="black",  #vertex.label.cex=.5, vertex.label.dist=0.5, 
@@ -138,7 +139,7 @@ IM_CP2<- delete_vertices(IM_CP, isolated_nodes)
 
 S <- vcount(IM_CP2)#number of species
 L <- ecount(IM_CP2) #number of links
-C <- ecount(IM_CP2) / vcount(IM_CP2)^2  #connectance
+#C <- ecount(IM_CP2) / vcount(IM_CP2)^2  #connectance
 
 ## assign attributes according to the taxon
 nodes_in_network<- nodes %>% filter(node_id%in%IMCP_edge$node_from | #create the nodelist according to the nodes present in the network
@@ -150,7 +151,7 @@ V(IM_CP2)$taxon<-nodes_in_network$taxon #create taxon attribute
 vcol=V(IM_CP2)$taxon
 vcol[vcol== "Plant"]="#33a02c"
 vcol[vcol=="Crop"]="#b15928"
-vcol[vcol=="Flower-visiting"]="#a6cee3"
+vcol[vcol=="Flower visitor"]="#a6cee3"
 vcol[vcol== "Aphid"]="#1f78b4"
 vcol[vcol=="Primary aphid parasitoid"]="#b2df8a"
 vcol[vcol=="Secondary aphid parasitoid"]="#fb9a99"
@@ -165,7 +166,7 @@ V(IM_CP2)$color=vcol
 
 
 #Plot 
-png("Graphs/IM_CP_network.png", width = 800, height = 600)
+pdf("Graphs/IN_network.pdf", width = 5, height = 7)
 IM_CPplot<-plot(IM_CP2,edge.arrow.size=.5, vertex.color= V(IM_CP2)$color, vertex.size=6, vertex.label=NA,
                vertex.frame.color="black", vertex.label.color="black", 
                #vertex.label.color="black",  #vertex.label.cex=.5, vertex.label.dist=0.5, 
